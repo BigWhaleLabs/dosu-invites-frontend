17d8daa1ef5a2fe4138cd61dac49615fd7b84e89
T-Damer <dpancov64@gmail.com>
2021-11-30 12:20:05 +0300

    completly removed icons

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,12 +1,5 @@
 import { CopyToClipboard } from 'react-copy-to-clipboard'
-import {
-  DefaultUi,
-  IconLibrary,
-  LoadingScreen,
-  Player,
-  Poster,
-  Video,
-} from '@vime/react'
+import { DefaultUi, LoadingScreen, Player, Poster, Video } from '@vime/react'
 import { TinyText } from 'components/Text'
 import { classnames } from 'classnames/tailwind'
 import { observer } from 'mobx-react-lite'
@@ -121,11 +114,6 @@ function Main() {
     <div className={mainBox}>
       <div className={playerBox}>
         <Player theme={theme} className={playerStyles}>
-          <IconLibrary
-            name="dosu-invite-icons"
-            resolver={(iconName) => `/icons/${iconName}.svg`}
-          />
-
           <Video poster="img/poster">
             <source data-src={videoLink} type="video/mp4" />
           </Video>
