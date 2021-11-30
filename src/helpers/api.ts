645814fd12e8b6d60588c0fa3c0c8a2c1dc9bac0
T-Damer <dpancov64@gmail.com>
2021-11-30 11:43:38 +0300

    updated backend link

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -2,7 +2,7 @@ import fetch from 'unfetch'
 
 export default async function getFramesToEthMap() {
   const framesToEthMap: { [frame: number]: string } = await (
-    await fetch('http://localhost:1337/video/data')
+    await fetch('http://localhost:1337/video/invites')
   ).json()
   return framesToEthMap
 }
