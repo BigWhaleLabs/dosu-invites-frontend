7ac8c4435f8565cf6b0d3ecf89bcd8a70d4643fd
T-Damer <dpancov64@gmail.com>
2021-12-17 21:39:23 +0300

    removed light theme, it's bad

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable sort-imports-es6-autofix/sort-imports-es6 */
 import '@vime/core/themes/default.css'
-import '@vime/core/themes/light.css'
 import './index.css'
 import { render } from 'preact'
 import App from './App'
