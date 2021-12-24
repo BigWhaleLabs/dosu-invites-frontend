894295e17343ed39a7432c88135b7ff01aad6d5c
T-Damer <dpancov64@gmail.com>
2021-12-24 09:09:50 +0300

    fixed relative imports problems

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable sort-imports-es6-autofix/sort-imports-es6 */
 import '@vime/core/themes/default.css'
-import './index.css'
+import 'index.css'
 import { render } from 'preact'
-import App from './App'
+import App from 'App'
 
 render(<App />, document.getElementById('root') as Element)
