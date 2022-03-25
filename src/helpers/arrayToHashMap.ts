069b60848e3e16e66cefd16398b9dee8e6a26214
Daniil Pankov <dpancov64@gmail.com>
2022-03-25 17:36:21 +0300

    Check currentAddress in MerkleTree from minted addresses  (#14)
    * Adding MetaMask, Benance, WalletConnect, Torus, Fortmatic, Authereum & Bitski
    
    * Some renamings
    
    * now it starts
    
    * added unified crypto wallet icon
    
    * fully operational web3Modal, now need to connect new wallets
    
    * now we can change the accounts
    
    * solved the problem with process
    
    * proper connection managment
    
    * it works, now need to fix theme settings and configure the wallets
    
    * added bitski
    
    * now button also used to change the account
    
    * fixed theme importing
    
    * configured 3+ wallets
    
    * updated readme
    
    * caching the provider to make the contract work after reload
    
    * clear cache so you can try another provider
    
    * fixed wallet icon
    
    * added coinbase
    
    * changed wallets icon and added coinbase wallet support
    
    * was able to connect bitski
    
    * updated readme
    
    * configure binance wallet
    
    * removed browsefy
    
    * dont use torus
    
    * run vite without --https
    
    * check tokenId after the contract is created, not before
    
    * lets only use dark theme, theme switching works poorly
    
    * dont use binance, because it doesn't support usual networks
    
    * use dark theme by default
    
    * now events will work, also the contract code will work
    
    * fixed glitches with appearing and disappearing button
    
    * fully fixed
    
    * yarn lock updated
    
    * added torus provider
    
    * moved links upper
    
    * use formatic rpc as .env var
    
    * actually import it
    
    * updated deps
    
    * upadted according to last researches
    
    * updated deps
    
    * add merkle tree support
    
    * looks like its done
    
    * lib update
    
    * updated deps
    
    * removed unnecessary code
    
    * re-run yarn
    
    * Update checkInMerkleTree.ts

--- src/helpers/arrayToHashMap.ts
+++
@@ -1,7 +0,0 @@
-export default function arrayToHashMap(array: string[], shift: number) {
-  const hashMap: { [x: number]: string } = {}
-  for (let i = 0; i < array.length; i++) {
-    hashMap[i + shift] = array[i]
-  }
-  return hashMap
-}
---
+++ src/helpers/checkInMerkleTree.ts
@@ -0,0 +1,21 @@
+import * as api from 'helpers/api'
+import { MerkleTree } from 'merkletreejs'
+import keecak256 from 'keccak256'
+
+export default async function checkInMerkleTree(addressFrame: number) {
+  const addresses = await api.getMintedAddresses()
+
+  const leafNodes = Object.values(addresses).map((address: string) =>
+    keecak256(address)
+  )
+  const merkleTree = new MerkleTree(leafNodes, keecak256, {
+    sortLeaves: true,
+    sortPairs: true,
+  })
+  const rootHash = merkleTree.getRoot()
+
+  const claimingAddress = leafNodes[addressFrame]
+  const hexProof = merkleTree.getHexProof(claimingAddress)
+
+  return merkleTree.verify(hexProof, claimingAddress, rootHash)
+}
