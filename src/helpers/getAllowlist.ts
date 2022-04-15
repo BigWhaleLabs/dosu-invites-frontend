deaca2692460b7a95c7f7047ac24d71a4630e9f2
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-04-15 09:47:01 -0700

    add jason's eth addresses

--- src/helpers/getAllowlist.ts
+++ src/helpers/getAllowlist.ts
@@ -1,3 +1,7 @@
 export default function getAllowlist() {
-  return ['0xbf74483DB914192bb0a9577f3d8Fb29a6d4c08eE']
+  return [
+    '0xbf74483DB914192bb0a9577f3d8Fb29a6d4c08eE',
+    '0x57BBF2657B7746779819664fB12960f66F627bA2',
+    '0x0E488e4a23cD8C67362AA716b5A2D45a9Cf65815',
+  ]
 }
