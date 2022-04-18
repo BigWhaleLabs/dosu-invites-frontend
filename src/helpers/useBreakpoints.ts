81ad31b4a39a5c9c93bde7a07954c3ca3cb1a0a2
Michel Erokhov <mixaileroxov@gmail.com>
2022-04-18 20:49:01 +0300

    fix useBreakpoints (#28)
    * fix useBreakpoints
    
    * update actions
    
    * update useBreakpoints

--- src/helpers/useBreakpoints.ts
+++ src/helpers/useBreakpoints.ts
@@ -3,13 +3,20 @@ import { useEffect, useState } from 'react'
 const mdSize = 768
 
 export default function useBreakpoints() {
-  const [size, setSize] = useState({ md: false })
   const [width, setWidth] = useState(window.innerWidth)
-  window.addEventListener('resize', () => setWidth(window.innerWidth))
 
   useEffect(() => {
-    setSize({ md: width > mdSize })
-  }, [size, width])
+    function resizer() {
+      setWidth(window.innerWidth)
+    }
 
-  return size
+    window.addEventListener('resize', resizer)
+    return () => {
+      window.removeEventListener('resize', resizer)
+    }
+  }, [])
+
+  return {
+    md: width > mdSize,
+  }
 }
