fac8444c8a753445659a18754cfc7d741df2fdc1
smyzin <smyzin.work@gmail.com>
2022-04-22 01:16:57 +0300

    Add more networks

--- src/models/Network.ts
+++ src/models/Network.ts
@@ -1,9 +1,18 @@
 enum NetworkChainIdToName {
-  '0x1' = 'homestead',
-  '0x3' = 'ropsten',
-  '0x4' = 'rinkeby',
-  '0x5' = 'goerli',
-  '0x2a' = 'kovan',
+  '0x1' = 'homestead', // Ethereum Mainnet
+  '0x3' = 'ropsten', // Ropsten Testnet
+  '0x4' = 'rinkeby', // Rinkeby Testnet
+  '0x5' = 'goerli', // Görli Testnet
+  '0x2a' = 'kovan', // Kovan Testnet
+  '0x0a' = 'optimism', //Optimism
+  '0x38' = 'bsc', //BSC
+  '0x61' = 'bsn', //BSN
+  '0x64' = 'xDai', //xDai
+  '0x89' = 'polygon', //Polygon
+  '0xa4b1' = 'arbitrum', //Arbitrum
+  '0xa86a' = 'avalanche', //Avalanche
+  '0x80' = 'heco-mainnet', // Huobi Mainnet
+  '0x0100' = 'heco-testnet', // Huobi Testnet
 }
 
 export default NetworkChainIdToName
