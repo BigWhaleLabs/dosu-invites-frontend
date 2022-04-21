480b501a88e0fea41cae1b4335a2da463272ce08
T-Damer <dpancov64@gmail.com>
2022-04-21 15:28:10 +0300

    added variables into env.ts

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -1,4 +1,8 @@
-// TODO: let's add all the env variables here
 export default {
   VITE_ETH_NETWORK: import.meta.env.VITE_ETH_NETWORK as string,
+  VITE_CONTRACT_ADDRESS: import.meta.env.VITE_CONTRACT_ADDRESS as string,
+  VITE_INFURA_ID: import.meta.env.VITE_INFURA_ID as string,
+  VITE_APP_NAME: import.meta.env.VITE_APP_NAME as string,
+  VITE_FORTMATIC_KEY: import.meta.env.VITE_FORTMATIC_KEY as string,
+  VITE_BITSKI_CLIENT_ID: import.meta.env.VITE_BITSKI_CLIENT_ID as string,
 }
