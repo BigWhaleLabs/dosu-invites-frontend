2654cba46473e0b29720e93d176a5b66e4d4fef7
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-04-22 17:34:58 -0700

    fix env var

--- src/helpers/defaultProvider.ts
+++ src/helpers/defaultProvider.ts
@@ -1,6 +1,6 @@
 import { providers } from 'ethers'
 import env from 'helpers/env'
 
-export default new providers.InfuraProvider(import.meta.env.VITE_ETH_NETWORK, {
+export default new providers.InfuraProvider(env.VITE_ETH_NETWORK, {
   projectId: env.VITE_INFURA_ID,
 })
