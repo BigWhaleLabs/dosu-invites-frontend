86ff127ae2fb00418b6932d71854fab329d8f552
T-Damer <dpancov64@gmail.com>
2022-04-26 18:41:39 +0300

    moved injectStyle outside render

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
-import { injectStyle } from 'react-toastify/dist/inject-style'
 import { useSnapshot } from 'valtio'
 import AppStore from 'stores/AppStore'
 import Main from 'pages/Main'
@@ -11,8 +10,6 @@ import ThemeProvider from 'components/ThemeProvider'
 
 function App() {
   const { theme } = useSnapshot(AppStore)
-
-  injectStyle()
 
   return (
     <ThemeProvider>
--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,8 @@ import 'index.css'
 
 import { render } from 'preact'
 import App from 'App'
+import { injectStyle } from 'react-toastify/dist/inject-style'
+
+injectStyle()
 
 render(<App />, document.getElementById('root') as Element)
