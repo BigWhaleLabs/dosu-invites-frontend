575d4933a7682e5c839a6a090ef87faf29017c2b
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-06-09 08:32:14 -0700

    up dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,7 @@
-/* eslint-disable sort-imports-es6-autofix/sort-imports-es6 */
 import 'index.css'
+import 'react-toastify/dist/ReactToastify.css'
 
 import { render } from 'preact'
 import App from 'App'
-import { injectStyle } from 'react-toastify/dist/inject-style'
-
-injectStyle()
 
 render(<App />, document.getElementById('root') as Element)
